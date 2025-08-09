6ef5762453caff7b24308d2bb19f08354ca35fcd
TingCris <shenhanting0509@gmail.com>
2025-08-09 15:31:15 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python -u train.py --cuda 0 --lr 0.001 --mu 0.0001 --eval_freq 100 --dim 200 --dataset DB15K --epochs 2000 --alpha_s 1e-5 --alpha_t 1e-5 --alpha_i 1e-5 --alpha_conf 1e-3 --alpha_cl 5e-5  > db15k.txt;
 python -u train.py --cuda 0 --lr 0.001 --mu 0.0001 --eval_freq 100 --dim 200 --dataset MKG-W --epochs 2000 --alpha_s 1e-4 --alpha_t 1e-4 --alpha_i 1e-4 --alpha_conf 1e-4 --alpha_cl 1e-4  > mkgw.txt;
-python -u train.py --cuda 0 --lr 0.001 --mu 0.0001 --eval_freq 100 --dim 200 --dataset MKG-Y --epochs 2000 --alpha_s 1e-3 --alpha_t 1e-3 --alpha_i 1e-3 --alpha_conf 1e-3 --alpha_cl 5e-4  > mkgy.txt;
+python -u train.py --cuda 0 --lr 0.001 --mu 0.0001 --eval_freq 100 --dim 200 --dataset MKG-Y --epochs 2000 --alpha_s 1e-3 --alpha_t 1e-3 --alpha_i 1e-3 --alpha_conf 1e-3 --alpha_cl 1e-3  > mkgy.txt;
 
